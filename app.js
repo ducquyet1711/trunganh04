4f2d7fc6e3c547a3f293b6c094bd8c73ac07a3e8
ducquyet1711 <gunnynhoc7@gmail.com>
2022-11-02 16:49:09 +0700

    update

--- app.js
+++ app.js
@@ -16,13 +16,15 @@ document.addEventListener('DOMContentLoaded', function(){
             rootTime.textContent = `${(hrs>9)?hrs:"0"+hrs}:${(min>9)?min:"0"+min}:${(sec>9)?sec:"0"+sec}`;
       } olock();
       var timer = setInterval(function(){olock()}, 1000);
-      document.querySelector("audio").setAttribute("src", `music/${music[Math.floor(Math.random()*music.length)]}.mp3`);
+      // document.querySelector("audio").setAttribute("src", `music/${music[Math.floor(Math.random()*music.length)]}.mp3`);
 
       document.getElementsByTagName("body")[0].insertAdjacentHTML(
             "beforeend",
             "<div id='mask'></div>"
       );
-
+      
 }, false);
 
 console.log(yourDate)
+
+
